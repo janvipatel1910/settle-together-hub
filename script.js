c15714b2a8e857278a032d1dd86c440908178c0c
janvipatel1910 <84894121+janvipatel1910@users.noreply.github.com>
2026-05-16 20:40:56 +0100

    Update Google Apps Script URL in script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const supportForm = document.querySelector("#support-form");
 const successMessage = document.querySelector("#success-message");
-const googleAppsScriptURL = "https://script.google.com/macros/s/AKfycbzvLug1eDlb0fet62H0VZnbd4Tspz1hlnwzV3u-qLl2v3wbuNK05HamkOptFs3rAM1R/exec";
+const googleAppsScriptURL = "https://script.google.com/macros/s/AKfycbxAePAEcgR-jgzprtsIdzKTze_3Qm8lLDGN_776l9Vhn70eV2ssVnbmdqrtCm7PfCD4/exec";
 const radioGroups = ["accommodation-support", "job-support", "food-support"];
 
 function showError(field) {
